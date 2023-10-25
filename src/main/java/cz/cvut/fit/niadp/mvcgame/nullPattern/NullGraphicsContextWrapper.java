400041195567dbecb38cc51df7c994a0ee52d083
Emil Poľák <emilpolak2212@gmail.com>
2023-10-25 21:28:42 +0200

    Added error output for missing graphics context

--- src/main/java/cz/cvut/fit/niadp/mvcgame/nullPattern/NullGraphicsContextWrapper.java
+++ src/main/java/cz/cvut/fit/niadp/mvcgame/nullPattern/NullGraphicsContextWrapper.java
@@ -1,22 +1,15 @@
 package cz.cvut.fit.niadp.mvcgame.nullPattern;
 
 import cz.cvut.fit.niadp.mvcgame.model.Position;
-import javafx.scene.Scene;
 
 public class NullGraphicsContextWrapper extends AbstractGraphicsContextWrapper {
     @Override
     public void clearRect(double x, double y, double w, double h) {
-        System.out.println("Method clearRect is not implemented.");
+        System.err.println("Method clearRect is not implemented.");
     }
 
     @Override
     public void drawImage(String imagePath, Position imagePosition) {
-        System.out.println("Method drawImage is not implemented.");
-    }
-
-    @Override
-    public Scene getScene() {
-        System.out.println("There is no scene initialized.");
-        return null;
+        System.err.println("Method drawImage is not implemented.");
     }
 }
