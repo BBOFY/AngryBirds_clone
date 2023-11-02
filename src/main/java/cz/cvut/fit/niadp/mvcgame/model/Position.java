72f93fb98fa39e6c44982a71c5a9c7585065b10f
Emil Poľák <emilpolak2212@gmail.com>
2023-11-02 23:29:09 +0100

    Renamed attributes to x and y. Removed Position class, because every position in games is described with vectors.

--- src/main/java/cz/cvut/fit/niadp/mvcgame/model/Position.java
+++ src/main/java/cz/cvut/fit/niadp/mvcgame/model/Position.java
@@ -1,36 +1,46 @@
-package cz.cvut.fit.niadp.mvcgame.model;
-
-public class Position {
-    private int dimX = 0;
-	private int dimY = 0;
-	
-	public Position() {
-	}
-
-	public Position(int posX, int posY) {
-		this.dimX = posX;
-		this.dimY = posY;
-	}
-
-	public int getX() {
-		return dimX;
-	}
-    
-    public int getY() {
-		return dimY;
-	}
-    
-    public void setY(int y) {
-		this.dimY = y;
-	}
-    
-    public void setX(int x) {
-		this.dimX = x;
-	}
-
-
-	public void add(Vector vector) {
-		dimX += vector.getX();
-		dimY += vector.getY();
-	}
-}
+//package cz.cvut.fit.niadp.mvcgame.model;
+//
+//import cz.cvut.fit.niadp.mvcgame.prototype.IClonable;
+//
+//public class Position implements IClonable {
+//    private int x = 0;
+//	private int y = 0;
+//
+//	public Position() {}
+//
+//	public Position(Position pos) {
+//		this.x = pos.x;
+//		this.y = pos.y;
+//	}
+//
+//	public Position(int posX, int posY) {
+//		this.x = posX;
+//		this.y = posY;
+//	}
+//
+//	public int getX() {
+//		return x;
+//	}
+//
+//    public int getY() {
+//		return y;
+//	}
+//
+//    public void setY(int y) {
+//		this.y = y;
+//	}
+//
+//    public void setX(int x) {
+//		this.x = x;
+//	}
+//
+//
+//	public void add(Vector2 vector) {
+//		x += vector.getX();
+//		y += vector.getY();
+//	}
+//
+//	public Position clone() {
+//		return new Position(x, y);
+//	}
+//}
---
+++ src/main/java/cz/cvut/fit/niadp/mvcgame/model/Vector2.java
@@ -0,0 +1,46 @@
+package cz.cvut.fit.niadp.mvcgame.model;
+
+import cz.cvut.fit.niadp.mvcgame.prototype.IClonable;
+
+public class Vector2 implements IClonable {
+    private int x = 0;
+    private int y = 0;
+
+    public Vector2() {}
+
+    public Vector2(Vector2 other) {
+        this.x = other.x;
+        this.y = other.y;
+    }
+
+    public Vector2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void add(Vector2 other) {
+        x += other.getX();
+        y += other.getY();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public Vector2 clone() {
+        return new Vector2(x, y);
+    }
+}
