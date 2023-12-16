95a36d82e4f2384cc1b43cb93b9b1b2dc0885269
Emil <polakemi@fit.cvut.cz>
2023-12-16 23:06:51 +0100

    Added test suite

--- src/test/java/cz/cvut/fit/niadp/mvcgame/model/GameModelTestSuit.java
+++ src/test/java/cz/cvut/fit/niadp/mvcgame/model/GameModelTestSuit.java
@@ -1,8 +1,9 @@
-//package cz.cvut.fit.niadp.mvcgame.model;
-//
-//import org.junit.runner.RunWith;
-//import org.junit.runners.Suite;
-//
-//@RunWith(Suite.class)
-//@Suite.SuiteClasses({GameModelBasicTest.class, GameModelMockedTest.class, GameModelReflectionTest.class})
-//public class GameModelTestSuit { }
+package cz.cvut.fit.niadp.mvcgame.model;
+
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GameModelBasicTest.class, GameModelMockedTest.class, GameModelReflectionTest.class})
+public class GameModelTestSuit { }
